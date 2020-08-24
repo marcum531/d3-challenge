dc97f34d4c7eea8e9346b03de4d23009b70be0e3
Nicole (Deborah) Marcum <marcum531@gmail.com>
2020-08-23 21:59:26 -0400

    second upload

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -12,13 +12,32 @@ var chartMargin = {
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight-chartMargin.top - chartMargin.bottom;
 
-var svg = d3
+var svg = d3.select("div")
     .select("#scatter")
     .append("svg")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
 
 var chartGroup = svg.append("g").attr("transform",`translate(${chartMargin.left}, ${chartMargin.top})`);
+
+var xaxis = "poverty";
+var yaxis = "obesity";
+
+function xScale(stateData, xaxis) {
+    var x = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.poverty)* 1.2])
+    .range([0, chartWidth]);
+
+    return x;
+}
+
+function yScale(stateData, yaxis) {
+    var y = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.obesity) * 1.2])
+    .range([chartHeight, 0]);
+
+    return y;
+}
 
 d3.csv("assets/data/data.csv").then(function(stateData){
     stateData.forEach(function(data){
@@ -30,6 +49,32 @@ d3.csv("assets/data/data.csv").then(function(stateData){
         data.smokes = +data.smokes;
         console.log(stateData);
     });
-});
+}).catch(function(error) {
+    console.log(error);
 
 
+
+
+svg.append("g")
+    .attr("transform", "translate(0," + chartHeight + ")")
+    .call(d3.axisBottom(x));
+
+
+
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+svg.append("g")
+    .selectAll("dot")
+    .data(stateData)
+    .enter()
+    .append("circle")
+        .attr("cx", function(d){
+            return x(d.poverty);
+        })
+        .attr("cy", function(d) {
+            return y(d.obesity);
+        })
+        .attr("r", 1.5)
+        .style("fill", d.abbr);
+    });
